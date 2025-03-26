dcbc4fe98fadff312d91ac21d89226eb5847c81f
Bastien-Lefrant <bastien.lefrant@icloud.com>
2025-03-26 19:13:37 +0100

    Initial Commit

---
+++ StoryLikeApp/StoryLikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryLikeAppApp.swift
+//  StoryLikeApp
+//
+//  Created by Bastien Lefrant on 26/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct StoryLikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoryLikeAppTests/StoryLikeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoryLikeAppTests.swift
+//  StoryLikeAppTests
+//
+//  Created by Bastien Lefrant on 26/03/2025.
+//
+
+import Testing
+@testable import StoryLikeApp
+
+struct StoryLikeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
